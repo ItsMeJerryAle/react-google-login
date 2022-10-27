6ff9bf12b8bffed474840fdd9a2e8bb0c1c234eb
Masterzerr <alejerry24@gmail.com>
2022-10-27 19:38:32 +0800

    jwt decode

--- google-auth/src/App.js
+++ google-auth/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 
 function App() {
   return (
       <div className="App">
         <header className="App-header">
+        <h1>Google Login Auth</h1>
         <GoogleLogin
           onSuccess={credentialResponse => {
-            console.log(credentialResponse);
+            console.log(credentialResponse.credential);
+            var decoded = jwt_decode(credentialResponse.credential);
+            console.log(decoded);
           }}
           onError={() => {
             console.log('Login Failed');
